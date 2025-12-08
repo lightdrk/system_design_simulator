e20082dfdce1dc62521ce09f514cf7b9b0abe98a
lightdrk <mohitrawatssj4@gmail.com>
2025-12-08 13:12:30 +0530

    started with buttons to add new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import { useState } from 'react'
 import ObjectComponent from './Components/object'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAdd } from "@fortawesome/free-solid-svg-icons";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-      <div className='w-screen h-screen bg-white flex flex-col justify-center align-center'>
-                <ObjectComponent title="REDIS"/>
-      </div>
-  )
+    const ObjectOnScreen = {};
+    return (
+        <>
+            <div className='p-2'>
+                <button
+                    className="cursor-pointer border-1 rounded-md p-1.5 hover:bg-sky-700 bg-sky-500 text-white "
+                >
+                    <FontAwesomeIcon icon={faAdd}></FontAwesomeIcon>
+                    Node
+                </button>
+            </div>
+            <div className='w-screen h-screen bg-white flex flex-col justify-center align-center border-1 border-dotted'>
+                <ObjectComponent title="REDIS" image="https://icon.icepanel.io/Technology/svg/Redis.svg"/>
+                <ObjectComponent title="REDIS" image="https://icon.icepanel.io/Technology/svg/Redis.svg"/>
+            </div>
+        </>
+    )
 }
 
 export default App
--- src/Components/object.jsx
+++ src/Components/object.jsx
@@ -1,27 +1,30 @@
 import { useState, useRef, useEffect } from 'react';
 
 
-export default function ObjectComponent({title}) {
+export default function ObjectComponent({title, image}) {
     const [data,setData] = useState();
     const ourComp = useRef();
-    const [left,setLeft] = useState('10');
-    const [top,setTop] = useState('10');
+    const [left, setLeft] = useState('10');
+    const [top, setTop] = useState('10');
     const [isDragging, setIsDragging] = useState(false);
-    const [xOffset,setXOffset] = useState('0');
-    const [yOffset,setYOffset] = useState('0');
+    const [xOffset, setXOffset] = useState('0');
+    const [yOffset, setYOffset] = useState('0');
 
     const handleDown = (e)=>{
         setIsDragging(true);
-        console.log(ourComp.current);
         console.log(e.clientX - ourComp.current.offsetLeft)
         setXOffset(e.clientX - ourComp.current.offsetLeft)
         setYOffset(e.clientY - ourComp.current.offsetTop)
     }
     const handleMovment = (e)=>{
-        console.log(isDragging);
         if (!isDragging) return
+        console.log("Moving ..")
+        console.log(e.clientX-xOffset);
         setTop(e.clientY-yOffset);
         setLeft(e.clientX-xOffset);
+    }
+    const handleRightClick = (e) => {
+        console.log('Right Click');
     }
 
     return (
@@ -30,10 +33,11 @@ export default function ObjectComponent({title}) {
             onMouseDown={handleDown} 
             onMouseMove={handleMovment} 
             onMouseUp={()=>{setIsDragging(false)}}
+            onContextMenu={handleRightClick}
             className={`absolute bg-transparent w-20 h-20 cursor-pointer`} 
             style={{ top: `${top}px`, left: `${left}px` }}title={title}
         >
-            <img className="" src="https://icon.icepanel.io/Technology/svg/Redis.svg" draggable={false}></img>
+            <img className="" src={image} draggable={false}></img>
         </button>
     );
 
